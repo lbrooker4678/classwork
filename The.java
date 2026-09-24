b7be67ed103183daae52c57964c0f85df398b428
lbrooker4678 <lbrooker4678@student.pps.net>
2026-09-24 06:56:13 -0700

    Add files via upload

---
+++ The.java
@@ -0,0 +1,10 @@
+public class The {
+    public static void main(String[] args) {
+    int number = 42; // Example number
+    if (number % 2 == 0) {
+        System.out.println("The number is even.");
+    } else {
+        System.out.println("The number is odd.");
+    }
+}
+}
---
+++ VarTest.java
@@ -0,0 +1,9 @@
+public class VarTest {
+    public static void main(String[] args) {
+        int hp = 100;
+        double luck = 3.5;
+        boolean isAlive = true;
+    
+        System.out.println("HP: " + hp + ", Luck: " + luck + ", Is Alive: " + isAlive);
+    }
+}
